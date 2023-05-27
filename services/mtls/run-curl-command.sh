f54cd81654e56fe2955d433b9f2b9f415a599072
nagi <nagi>
2023-05-28 00:12:22 +0100

    really changed port number for testing

--- services/mtls/run-curl-command.sh
+++ services/mtls/run-curl-command.sh
@@ -2,7 +2,7 @@
 set -eux
 
 cd `dirname $0`
-curl https://localhost:8443 \
+curl https://localhost:443 \
      --cacert ./certs-folder/ca.crt \
      --key ./certs-folder/client.key \
      --cert ./certs-folder/client.crt
