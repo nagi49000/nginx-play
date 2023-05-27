8d20e2d6309f9de72b719621f2bb482d61818f60
nagi <nagi>
2023-05-28 00:22:26 +0100

    simplified docker steps

--- services/mtls/run-curl-command.sh
+++ services/mtls/run-curl-command.sh
@@ -2,8 +2,7 @@
 set -eux
 
 cd `dirname $0`
-sleep 2
-curl https://localhost:443 \
+curl https://localhost:8443 \
      --cacert ./certs-folder/ca.crt \
      --key ./certs-folder/client.key \
      --cert ./certs-folder/client.crt
